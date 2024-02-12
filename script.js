f6a89d35f1a4e4a5397e0366590e6e14d39a1433
Martosky <imoniaromartins@yahoo.com>
2024-02-12 02:45:12 +0100

    did some jscript function

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const bgColor = document.getElementById("content-body");
 const button = document.getElementById("dark-bright");
 
-button.addEventListener("click", changeBgColor);
+
+button.addEventListener("click",  changeBgColor,);
 
 function changeBgColor(){
-    bgColor.classList.toggle("dark")
-    button.style.color = "white"
+    bgColor.classList.toggle("dark");
+    const changeText = document.getElementsByClassName("change");
+    for (let i = 0; i < changeText.length; i++){
+        if (changeText[i].style.color == "white"){
+            changeText[i].style.color = "black"
+        }else {
+            changeText[i].style.color = "white"
+        }
+    }
+}
+
+function textColor(){
+    const changeText = document.getElementsByClassName("change");
+    for (let i = 0; i < changeText.length; i++){
+        if (changeText[i].style.color == "black"){
+            changeText[i].style.color = "white"
+        }else {
+            changeText[i].style.color = "black"
+        }
+    }
 }
